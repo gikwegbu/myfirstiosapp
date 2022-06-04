50d27fcf6575d45f776c36bebce317bd26febfdc
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2022-06-04 22:41:39 +0100

    Initial Commit

---
+++ iosapp/iosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosappApp.swift
+//  iosapp
+//
+//  Created by gikwegbu on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
