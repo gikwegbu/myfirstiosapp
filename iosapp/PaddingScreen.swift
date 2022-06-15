36933feead10de9a83cb52bd2005a38a367bee53
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2022-06-15 10:12:47 +0100

    Learned about padding
    1. Here I built a simple card with my understanding of vstack and padding,

--- iosapp/PaddingScreen.swift
+++ iosapp/PaddingScreen.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct PaddingScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            .padding(.bottom, 20)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("This is the most used introductory sentence in every programming language.")
+                .font(.title2)
+            
+                .multilineTextAlignment(.leading)
+        }
+        .padding()
+        .padding(.vertical, 10)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(
+                    color: Color.black.opacity(0.3),
+                    radius: 10,
+                    x: 5, y: 10)
+        )
+        .padding()
+
+        
     }
 }
 
